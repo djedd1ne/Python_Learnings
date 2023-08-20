472a60cc09d6f6fb13b460ebf1056c22e995bfa5
djedd1ne <mekki.djallaleddine@gmail.com>
2023-08-20 11:43:55 +0200

    day2 question 1,2,3

---
+++ variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# name, age, average score
+person1 = ["Ali", 20, 18.0]
+perosn2 = ["Alex", 23, 15.6]
+print(person1, "\n" + str(perosn2), "\n")
+
+#concatenate strings
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
+print("concatenated string is: ", string1 + string2, '\n')
+
+#print list using index
+fruits = ["banana", "apple", "peach"]
+for i in fruits:
+    print(i)
